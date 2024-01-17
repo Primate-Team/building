4365fe8a16374b8afebe8719c6a26bf90c259928
Samsonium <tdesu@vk.com>
2024-01-17 14:08:27 +0300

    Initial commit

---
+++ src/main/java/ru/samsonium/primate/building/PrimateBuilding.java
@@ -0,0 +1,17 @@
+package ru.samsonium.primate.building;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PrimateBuilding extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
